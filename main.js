bdb3357decc32c9d5e591d35c16574bb83a6c58c
Aaron Anderson <aaronlynanderson@gmail.com>
2017-08-10 11:09:13 -0400

    Calculator basic functionality complete

---
+++ main.js
@@ -0,0 +1,19 @@
+let calculator = document.querySelector('.calculator');
+let input = document.querySelector('input');
+let result = 0
+console.log(input);
+calculator.addEventListener('click',(e)=>{
+  if (e.target !== e.currentTarget){
+    if (e.target.innerHTML === "="){
+      result = eval(input.value);
+      input.value = result;
+    } else if (e.target.innerHTML === "C"){
+      input.value = '';
+    } else if (e.target.innerHTML === "x"){
+      input.value += "*";
+    } else {
+      console.log(e.target.innerHTML);
+      input.value += e.target.innerHTML;
+    }
+  }
+})
